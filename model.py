77c923aefbaa4e622b59e421ea22a7b5a72c5ef9
SpaceX3 <zhong.larry.138@gmail.com>
2026-03-18 18:14:23 +0800

    add linea

--- model.py
+++ model.py
@@ -115,7 +115,7 @@ class scoring_head(nn.Module):
     def model_forward(self, x, hidden_state=None, first_frame=False, back=False):
         # x shape: B x 2 (a & v) x D
 
-        x = self.linear1(x)
+        # x = self.linear1(x)
 
         if back:
             batch_size = x.shape[0]
